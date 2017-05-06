808c5ab1db8bf7766d6bcce2ddc84f58765c2f2f
Alyssa Ramsey <aly.m.ramsey@gmail.com>
2017-05-06 14:42:48 -0500

    almost finished rendering stuff on run page

--- Components/Run.js
+++ Components/Run.js
@@ -15,52 +15,55 @@ import MapView from 'react-native-maps';
 
 //added for react-redux
 import {connect} from 'react-redux'
-import {fetchRunnerCoords} from './storeAndReducer'
+import {fetchNearbyRoutes} from './storeAndReducer'
 
 
 class Run extends Component {
+  // constructor(){
+  //   super();
+  //   this.state = Object.assign({}, this.props)
+  // }
 
-  addRunnerCoords(evt){
-    console.log("EVT ON MAP", evt)
-    var coords = Math.floor(Math.random * 100)
-    this.props.fetchRunnerCoords(coords)
+  componentWillMount(){
+    // console.log("will this work?")
+    this.props.fetchNearbyRoutes();
   }
-
 
   render() {
 
 
   	const { navigate } = this.props.navigation;
 
-    console.log("this.state is", this.state)
-    console.log("this.props is", this.props)
-
     const gotoRouteSelect = () => Actions.routeSelectPage({text: 'this goes to route select page!'});
 
+    //
+    // const testRoutesArr=//dummy data... delete this once we are able to get routes from props (and from backend)
+    // [
+    //   {
+    //     id: 1,
+    //     coords: [{latitude: 37, longitude: -122},{latitude: 36.5, longitude: -121},{latitude: 36.25, longitude: -119.5}],//this is routes array
+    //     routetimes: [
+    //       {timesArr: [0,7,16,24], user: {username: 'Alyssa'}},//these are times arrays associated with routes, and the times arrays also have their associated user
+    //       {timesArr: [0,8,16,25], user: {username: 'Gabi'}},
+    //       {timesArr: [0,5,10,19], user: {username: 'Charles'}}
+    //     ],
+    //   },
+    //   {
+    //     id: 2,
+    //     coords: [{latitude: 35, longitude: -118},{latitude: 35.75, longitude: -119.75},{latitude: 35.5, longitude: -119.5}],//this is routes array
+    //     routetimes: [
+    //       {timesArr: [0,7,16,24], user: {username: 'Alyssa'}},
+    //       {timesArr: [0,8,16,25], user: {username: 'Gabi'}},
+    //       {timesArr: [0,5,10,19], user: {username: 'Charles'}}
+    //       ],
+    //   }
+    // ]
 
-    const testRoutesArr=//dummy data... delete this once we are able to get routes from props (and from backend)
-    [
-      {
-        id: 1,
-        coords: [{latitude: 37, longitude: -122},{latitude: 36.5, longitude: -121},{latitude: 36.25, longitude: -119.5}],//this is routes array
-        routetimes: [
-          {timesArr: [0,7,16,24], user: {username: 'Alyssa'}},//these are times arrays associated with routes, and the times arrays also have their associated user
-          {timesArr: [0,8,16,25], user: {username: 'Gabi'}},
-          {timesArr: [0,5,10,19], user: {username: 'Charles'}}
-        ],
-      },
-      {
-        id: 2,
-        coords: [{latitude: 35, longitude: -118},{latitude: 35.75, longitude: -119.75},{latitude: 35.5, longitude: -119.5}],//this is routes array
-        routetimes: [
-          {timesArr: [0,7,16,24], user: {username: 'Alyssa'}},
-          {timesArr: [0,8,16,25], user: {username: 'Gabi'}},
-          {timesArr: [0,5,10,19], user: {username: 'Charles'}}
-          ],
-      }
-    ]
+    // const routesArr= testRoutesArr;
+    let routesArr = this.props.nearbyRoutes;
 
-    const routesArr= testRoutesArr;
+    console.log("THIS PROPS NB ROUTES", this.props.nearbyRoutes)
+
     // const routesArr= navigation.state.params.routesArr; //uncomment this once we are able to get the routes from props
 
     // const polyLineArr=[{latitude: 37, longitude: -122},{latitude: 36, longitude: -119}];  //example of something you can pass into Polyline as coordinates (as props)
@@ -94,25 +97,38 @@ class Run extends Component {
        	 	<MapView style={styles.map}>
 
           {routesArr.map(routeObj=>{
+            //routeObj looks like { id: 1, coords: [[37, -122],[3,4],[5,6]] }
+
+
+
             return(
+
 
               <View key={routeObj.id} >
 
-                <MapView.Polyline coordinates={routeObj.coords} strokeColor='green' strokeWidth= {2} />
+               <MapView.Polyline
+                 coordinates={
 
-                <MapView.Marker
+
+                   routeObj.coords.map(function(coordPair){
+                     return {latitude: coordPair[0], longitude: coordPair[1]}
+                    })
+                   }
+                   strokeColor='green'
+                   strokeWidth= {2}
+                 />
+
+                {/* <MapView.Marker
                   coordinate={routeObj.coords[0]}
                   pinColor='red'
                   title='Start'
-                  description={routeObj.routetimes.map(routetime=>{
-                    return routetime.user.username+', time: '+routetime.timesArr[routetime.timesArr.length-1];
-                  }).join(', ')}
+
                 />
                 <MapView.Marker
                   coordinate={routeObj.coords[routeObj.coords.length-1]}
                   pinColor='blue'
                   title='End'
-                />
+                /> */}
               </View>
             )
           })}
@@ -126,11 +142,11 @@ class Run extends Component {
   }
 }
 
-const mapDispatchToProps = {fetchRunnerCoords}
+const mapDispatchToProps = {fetchNearbyRoutes}
 
 function mapStateToProps(state){
   return {
-    runnerCoords: state.runnerCoords
+    nearbyRoutes: state.nearbyRoutes
   }
 }
 
@@ -139,3 +155,11 @@ var ConnectedRun = connect(mapStateToProps, mapDispatchToProps)(Run)
 
 
 export default ConnectedRun
+
+
+
+
+
+///////*}// description={routeObj.routetimes.map(routetime=>{
+//   return routetime.user.username+', time: '+routetime.timesArr[routetime.timesArr.length-1];
+// }).join(', ')}
