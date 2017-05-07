abb9657a2277198dba347bfb0b52b2acc4a67df4
Alyssa Ramsey <aly.m.ramsey@gmail.com>
2017-05-06 19:16:50 -0500

    Update storeAndReducer.js

--- Components/storeAndReducer.js
+++ Components/storeAndReducer.js
@@ -13,7 +13,6 @@ import axios from 'axios';
 const SET_USER = 'SET_USER'
 const SET_USER_LOCATION = 'SET_USER_LOCATION'
 const SET_NEARBY_ROUTES = 'SET_NEARBY_ROUTES'
-
 const SET_SELECTED_ROUTE = 'SET_SELECTED_ROUTE'
 
 // const SET_RUNNER_COORDS = 'SET_ALL_COORDS'
@@ -71,22 +70,6 @@ export const fetchUserLocation = location => {
   }
 }
 
-export const fetchNearbyRoutes = () => {
-  return dispatch => {
-    axios.get('http://localhost:3000/api/runroutes')
-    .then(res => res.data)
-    .then(routesData => {
-      let formattedRouteData = routesData.map(routeWCoords => {
-        let formattedCoordsPerRoute = routeWCoords.coords.map(coordPair => {
-          return coordPair.map(coord => {
-            return +coord
-          })
-        })
-        return { id: routeWCoords.id, coords: formattedCoordsPerRoute}
-      })
-      return dispatch(setNearbyRoutes(formattedRouteData))
-    })
-    .catch(console.log)
 
 export const addNewRoute = (convCoords, userId, timesArr, startTime, endTime) => {
   return dispatch => {
@@ -98,8 +81,6 @@ export const addNewRoute = (convCoords, userId, timesArr, startTime, endTime) =>
 
   }
 }
-
-
 
 export const fetchSelectedRoute = selectedRouteId => {
   return dispatch => {
@@ -134,11 +115,6 @@ export const fetchSelectedRoute = selectedRouteId => {
 // }
 
 
-export const fetchUserLocation = location => {
-  return dispatch => {
-    return dispatch(setUserLocation(location))
-  }
-}
 
 export const fetchNearbyRoutes = (region) => {
   // console.log("or this?!")
@@ -175,47 +151,6 @@ http://localhost:3000/api/runroutes/?latitude=35&longitude=-119&latitudeDelta=3&
   }
 }
 
-export const fetchSelectedRouteCoords = selectedRoute => {
-  return dispatch => {
-    axios.get('/api/ROUTES/IDorSOMETHING')
-    .then(res => res.data)
-    .then(routeCoords => {
-      return dispatch(setSelectedRouteCoords(routeCoords))
-    })
-    .catch(console.log)
-  }
-}
-
-export const fetchSelectedRouteTimes = selectedRoute => {
-  return dispatch => {
-    axios.get('/api/ROUTESTIMES/IDorSOMETHING')
-    .then(res => res.data)
-    .then(routeTimes => {
-      return dispatch(setSelectedRouteTimes(routeTimes))
-    })
-    .catch(console.log)
-  }
-}
-
-export const createNewRoute = (newRouteCoords, newRouteTimes) => {
-  return dispatch => {
-    axios.post('/api/ROUTESorSomething', routeCoords)
-    .then(res => res.data)
-    .then(newRoute => {
-      return newRoute
-    })
-    .then(newRouteCoords => {
-      axios.post('/api/ROUTESTIMESorSomething', (newRouteTimes, newRoute.id))
-      .then(res => res.data)
-      .then(newRouteTimes => newRouteTimes)
-    })
-    .then(newRouteCoords => {
-      return dispatch(setSelectedRouteCoords(newRoute))
-    })
-    .catch(console.log)
-  }
-}
-
 
 
 
@@ -245,6 +180,7 @@ function reducer(state = initialState, action){
       nextState.nearbyRoutes = action.nearbyRoutes;
       break;
     case SET_SELECTED_ROUTE:
+      nextState.selectedRoute = action.selectedRoute
       break;
     default:
       return state;
