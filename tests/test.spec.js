27cd5ea8f13110e5258a3ed405e1d4e4910bc5a9
Charles <charles.m.long43@gmail.com>
2017-05-12 12:11:03 -0500

    added tests

---
+++ tests/test.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import chai, {expect} from 'chai';
+
+describe('TEST', () => {
+
+  describe('should asdfasdf', () => {
+    var a= 2
+    beforeEach('asdf', () => {
+    });
+
+    it('asdf', () => {
+      expect(a+a).to.be.equal(4)
+
+    });
+
+  });
+
+});
